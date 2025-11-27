9457add994e931bb92689a600207ea707580416a
UdeshaRashmi <rashmiparanamana1999@gmail.com>
2025-11-27 18:09:38 +0530

    update api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,23 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/userController');
 const deckController = require('../controllers/deckController');
-const cardController = require('../controllers/cardController');
-const achievementController = require('../controllers/achievementController');
+
+router.get('/users', userController.getAllUsers);
+router.get('/users/:id', userController.getUserById);
+router.post('/users', userController.createUser);
+router.put('/users/:id', userController.updateUser);
+router.delete('/users/:id', userController.deleteUser);
 
 router.get('/decks', deckController.getAllDecks);
 router.get('/decks/:id', deckController.getDeckById);
 router.post('/decks', deckController.createDeck);
 router.put('/decks/:id', deckController.updateDeck);
 router.delete('/decks/:id', deckController.deleteDeck);
-
-router.get('/decks/:deckId/cards', cardController.getCardsByDeck);
-router.post('/decks/:deckId/cards', cardController.createCard);
-router.put('/decks/:deckId/cards/:cardId', cardController.updateCard);
-router.delete('/decks/:deckId/cards/:cardId', cardController.deleteCard);
-
-router.post('/study-session', achievementController.addStudySession);
-router.get('/achievements', achievementController.getUserAchievements);
-router.get('/user-stats', achievementController.getUserStats);
-router.get('/leaderboard', achievementController.getLeaderboard);
 
 module.exports = router;
